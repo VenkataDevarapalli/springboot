b79cd842a8935551622b4c4a2f2d152d053886f1
Venkat <krishnarjy@gmail.com>
2021-03-13 10:43:28 +0000

    first commit

---
+++ demo/src/main/java/com/example/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.domain.Employee;
+
+@RestController
+@RequestMapping(value="/post")
+public class PostController {
+	
+	@PostMapping(value="/method1")
+	public Employee saveEmployee(@RequestBody Employee e) {
+		System.out.println("In the save method");
+		System.out.println(e.getName());
+		System.out.println(e.getAge());
+		return e;
+		
+	}
+
+}
